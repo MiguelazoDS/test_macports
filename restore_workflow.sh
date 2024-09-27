ed55ed5032a3e2aec2a53df5a5e580450d0d8bf8
Cazajous Miguel A. <migue7490@gmail.com>
2024-09-27 16:29:18 -0300

    Test

---
+++ restore_workflow.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Restoring GitHub Actions workflows"
+directories=".github/actions .github/workflows"
+git checkout master -- ${directories}
+if [ $? -eq 0 ]; then
+  echo "Successfully restored ${directories} directories"
+else
+  echo "Couldn't restore ${directories}."
+  exit 1
+fi
